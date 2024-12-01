e49c95600a54d679b35927c86f1e79b8035e62ab
Omar Nasser <omarnaser1997@gmail.com>
2024-12-01 21:45:39 +0300

    .....

--- example/create_app.sh
+++ example/create_app.sh
@@ -7,7 +7,8 @@ yes | fastlane spaceauth -u o.nasser@eltgcc.com
 export FASTLANE_PASSWORD="kvlk-lpbs-nuaf-xsyq"
 
 ## Run fastlane produce with the necessary options
-fastlane produce -a com.elt.productt.widgett -q "Product widget" -b com.elt.productt.widgett <<EOF
+fastlane produce<<EOF
+Product widget test
 1
 EOF
 
